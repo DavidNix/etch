c1d87872b2bccfea6be394968e4b3ae5ceeb1293
David Nix <davidnix81@gmail.com>
2015-09-08 17:46:03 -0600

    implement static files

--- main.go
+++ main.go
@@ -2,32 +2,58 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
+
+type config struct {
+	appName string
+	author  string
+	year    string
+}
 
 func main() {
 	fmt.Println("Enter your app's name: ")
 	var appName string
 	fmt.Scanln(&appName)
 
-	exitIf(createDirStructure(appName))
+	// fmt.Println("Enter author's name for license: ")
+	// var author string
+	// fmt.Scanln(&author)
 
-	// a := MustAsset("templates/Makefile")
-	// fmt.Print(string(a))
+	dir := createDirStructure(appName)
+	exitIf(os.Chdir(dir))
+
+	writeStatic("Makefile")
+	writeStatic("dev.env")
+
+	fmt.Println(appName + "'s workspace is complete.")
 }
 
 func exitIf(err error) {
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err, "Exiting...")
 		os.Exit(1)
 	}
 }
 
-func createDirStructure(app string) error {
-	if app == "" {
-		return fmt.Errorf("App name cannot be blank.")
+func createDirStructure(name string) string {
+	if name == "" {
+		exitIf(fmt.Errorf("App name cannot be blank."))
 	}
 	sep := string(filepath.Separator)
-	return os.MkdirAll("."+sep+app+sep+"src"+sep+app, 0777)
+	path := "." + sep + name + sep + "src" + sep + name
+	exitIf(os.MkdirAll(path, 0777))
+	return path
+}
+
+func template(name string) []byte {
+	templ, err := Asset("templates/" + name)
+	exitIf(err)
+	return templ
+}
+
+func writeStatic(name string) {
+	exitIf(ioutil.WriteFile(name, template(name), 0644))
 }
